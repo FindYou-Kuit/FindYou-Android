dd509ef01f617df479441e4386566c90f8990a7d
MinseoSONG <mins041600@naver.com>
2025-01-02 04:23:00 +0900

    [feat] #7: 구조, 신고 동물 바 구현

--- app/src/main/java/com/example/findu/presentation/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/findu/presentation/ui/home/HomeFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.example.findu.R
 import com.example.findu.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -19,7 +20,20 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+        setupTodayData()
+
         return binding.root
+    }
+
+    private fun setupTodayData() {
+        val rescueCount = 2 // 오늘 구조된 동물 dummy 값
+        binding.tvHomeTodayRescueNum.text =
+            getString(R.string.home_today_bar_rescue_num, rescueCount)
+
+        val reportCount = 6 // 오늘 신고된 동물 dummy 값
+        binding.tvHomeTodayReportNum.text =
+            getString(R.string.home_today_bar_report_num, reportCount)
     }
 
     override fun onDestroyView() {
