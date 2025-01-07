89a0b93dc4a988bffb4433b4f7930d2d083ea855
MinseoSONG <mins041600@naver.com>
2025-01-08 04:51:03 +0900

    [feat] #7: 보호소 조회 섹션

---
+++ app/src/main/java/com/example/findu/presentation/ui/home/HomeProtectAdapter.kt
@@ -0,0 +1,50 @@
+package com.example.findu.presentation.ui.home
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.findu.databinding.ItemHomeRvBinding
+import com.example.findu.presentation.model.HomeRv
+import com.example.findu.presentation.type.AnimalStateType
+
+class HomeProtectAdapter(
+    private val items: List<HomeRv>
+) : RecyclerView.Adapter<HomeProtectAdapter.HomeViewHolder>() {
+
+    inner class HomeViewHolder(private val binding: ItemHomeRvBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: HomeRv) {
+            binding.tvHomeName.text = item.name
+            binding.tvHomeDate.text = item.date
+            binding.tvHomeLocation.text = item.location
+
+            binding.chipHomeAnimalState.text = item.type
+            val stateType = AnimalStateType.entries.find { it.state == item.type }
+            if (stateType != null) {
+                binding.chipHomeAnimalState.setTextColor(
+                    binding.root.context.getColor(stateType.textColor)
+                )
+                binding.chipHomeAnimalState.chipBackgroundColor =
+                    binding.root.context.getColorStateList(stateType.backgroundChipColor)
+            }
+
+            Glide.with(binding.root.context)
+                .load(item.imageUrl)
+                .into(binding.ivHome)
+
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeViewHolder {
+        val binding =
+            ItemHomeRvBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return HomeViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: HomeViewHolder, position: Int) {
+        holder.bind(items[position])
+    }
+
+    override fun getItemCount(): Int = items.size
+}
