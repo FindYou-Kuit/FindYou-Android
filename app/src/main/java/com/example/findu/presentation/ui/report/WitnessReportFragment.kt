a7ef061c64bf9d482b600aa64e22abbf281bc5a5
IKSEONGKU <127182222+IKSEONGKU@users.noreply.github.com>
2025-01-09 02:26:46 +0900

    [feat] #13: 신고 - 이미지 업로드 하는 리사뷰 어댑터 구현

--- app/src/main/java/com/example/findu/presentation/ui/report/WitnessReportFragment.kt
+++ app/src/main/java/com/example/findu/presentation/ui/report/WitnessReportFragment.kt
@@ -6,15 +6,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
-import com.example.findu.R
-import com.example.findu.databinding.FragmentHomeBinding
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.findu.databinding.FragmentWitnessReportBinding
-import com.example.findu.presentation.ui.home.HomeViewModel
+import com.example.findu.presentation.ui.report.adapter.ReportImageAdapter
+import com.example.findu.presentation.ui.report.model.DummyImages
+import com.example.findu.presentation.ui.report.model.ReportType
 
 class WitnessReportFragment : Fragment() {
     private var _binding: FragmentWitnessReportBinding? = null
     private val binding get() = _binding!!
     private val reportViewModel by viewModels<ReportViewModel>()
+
+    private lateinit var reportImageAdapter: ReportImageAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,6 +25,22 @@ class WitnessReportFragment : Fragment() {
     ): View? {
         _binding = FragmentWitnessReportBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupUploadImageRecyclerView()
+
+    }
+
+    private fun setupUploadImageRecyclerView() {
+        reportImageAdapter = ReportImageAdapter(ReportType.WITNESS).apply {
+            submitList(DummyImages.dummyImageUris)
+        }
+        with(binding.rvWitnessReportImages) {
+            adapter = reportImageAdapter
+            layoutManager = LinearLayoutManager(context, androidx.recyclerview.widget.LinearLayoutManager.HORIZONTAL, false)
+        }
     }
 
     override fun onDestroyView() {
---
+++ app/src/main/java/com/example/findu/presentation/ui/report/adapter/ReportImageAdapter.kt
@@ -0,0 +1,124 @@
+package com.example.findu.presentation.ui.report.adapter
+
+import android.annotation.SuppressLint
+import android.net.Uri
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import androidx.transition.Visibility
+import com.bumptech.glide.Glide
+import com.example.findu.databinding.ItemReportDefaultImageBinding
+import com.example.findu.databinding.ItemReportUploadedImageBinding
+import com.example.findu.presentation.type.report.SpeciesType
+import com.example.findu.presentation.ui.report.model.ReportType
+
+class ReportImageAdapter(
+    val reportType: ReportType
+) : ListAdapter<Uri, RecyclerView.ViewHolder>(diffUtil) {
+
+    inner class DefaultImageViewHolder(private val binding: ItemReportDefaultImageBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        @SuppressLint("SetTextI18n")
+        fun bind(count: Int) {
+            // 사진 개수 카운트
+            binding.tvReportDefaultCount.text = (count - 1).toString()
+
+            binding.root.setOnClickListener {
+                // TODO : 사진 이미지 업로드 기능 설정
+            }
+        }
+    }
+
+    inner class ImageViewHolder(private val binding: ItemReportUploadedImageBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(uri: Uri) {
+            when (reportType) {
+                // 목격 신고
+                ReportType.WITNESS -> {
+                    binding.btnReportUploadedAiDistinction.setOnClickListener {
+                        // TODO : AI 버튼 클릭 이벤트 설정
+                    }
+                }
+                // 실종 신고
+                ReportType.MISSING -> {
+                    binding.btnReportUploadedAiDistinction.visibility = View.GONE
+                }
+            }
+
+            Glide.with(binding.root)
+                .load(uri)
+                .into(binding.ivReportUploadedImage)
+
+        }
+    }
+
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (holder) {
+            is DefaultImageViewHolder -> holder.bind(currentList.size)
+            is ImageViewHolder -> holder.bind(currentList[position])
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder =
+        when (viewType) {
+            // 0 번째 아이템
+            DEFAULT_VIEW_TYPE -> {
+                Log.d("ReportImageAdapter", "viewType: $viewType")
+
+                DefaultImageViewHolder(
+                    ItemReportDefaultImageBinding.inflate(
+                        LayoutInflater.from(parent.context),
+                        parent,
+                        false
+                    )
+                )
+            }
+
+            // IMAGE_VIEW_TYPE, 정상적으로 업로드될 이미지
+            else -> {
+                Log.d("ReportImageAdapter", "viewType: $viewType")
+
+                ImageViewHolder(
+                    ItemReportUploadedImageBinding.inflate(
+                        LayoutInflater.from(parent.context),
+                        parent,
+                        false
+                    )
+                )
+            }
+
+        }
+
+    override fun getItemViewType(position: Int): Int =
+        if (position == 0) DEFAULT_VIEW_TYPE // 0번째 아이템은 DefaultImageViewHolder
+        else IMAGE_VIEW_TYPE
+
+    companion object {
+
+        private const val DEFAULT_VIEW_TYPE = 0
+        private const val IMAGE_VIEW_TYPE = 1
+
+        val diffUtil = object : DiffUtil.ItemCallback<Uri>() {
+
+            override fun areItemsTheSame(
+                oldItem: Uri,
+                newItem: Uri
+            ): Boolean {
+                return oldItem == newItem
+            }
+
+            override fun areContentsTheSame(
+                oldItem: Uri,
+                newItem: Uri
+            ): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+}
