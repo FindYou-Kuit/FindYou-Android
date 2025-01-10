e147826506d88a82ee5ca1d87de5a612e3f30a0d
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2025-01-11 00:47:12 +0900

    [feat] #13: 신고 - 품종 어댑터 연결
    - setAdapter
    - background 설정

--- app/src/main/java/com/example/findu/presentation/ui/report/MissingReportFragment.kt
+++ app/src/main/java/com/example/findu/presentation/ui/report/MissingReportFragment.kt
@@ -1,6 +1,7 @@
 package com.example.findu.presentation.ui.report
 
 import android.content.Context
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -9,17 +10,22 @@ import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.view.ViewTreeObserver.OnScrollChangedListener
+import android.view.inputmethod.InputMethodManager
 import android.widget.ArrayAdapter
 import androidx.annotation.LayoutRes
+import androidx.annotation.RequiresApi
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.findu.R
 import com.example.findu.databinding.FragmentMissingRepostBinding
+import com.example.findu.databinding.ItemReportBreedsBinding
 import com.example.findu.presentation.ui.report.adapter.ReportImageAdapter
 import com.example.findu.presentation.ui.report.model.ReportDummys
 import com.example.findu.presentation.type.report.ReportType
-import com.example.findu.presentation.ui.report.adapter.ReportBreedSpinnerAdapter
+import com.example.findu.presentation.ui.report.adapter.ReportBreedAdapter
+import com.example.findu.presentation.util.ViewUtils.dpToPx
 import dagger.hilt.android.qualifiers.ApplicationContext
 
 class MissingReportFragment : Fragment() {
@@ -28,6 +34,7 @@ class MissingReportFragment : Fragment() {
     private val reportViewModel by viewModels<ReportViewModel>()
 
     private lateinit var reportImageAdapter: ReportImageAdapter
+    private lateinit var breedAdapter: ArrayAdapter<String>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -42,6 +49,23 @@ class MissingReportFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupUploadImageRecyclerView()
+        setUpBreedsAdapter(requireContext())
+
+    }
+
+
+    private fun setUpBreedsAdapter(context: Context) {
+        breedAdapter = ReportBreedAdapter(
+            requireContext(),
+            ReportDummys.dummyBreeds
+        )
+
+        with(binding.actvMissingReportBreed) {
+            setAdapter(breedAdapter)
+            setDropDownBackgroundResource(R.drawable.bg_bottom_radius_8_g4)
+
+
+        }
     }
 
     private fun setupUploadImageRecyclerView() {
@@ -58,5 +82,11 @@ class MissingReportFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    companion object {
+        private const val SCROLL_OFFSET = 258
+        private const val DROP_DOWN_HEIGHT = 248
+        private const val DROP_DOWN_MAX_COUNT = 8
     }
 }
