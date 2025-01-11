15d27d35e36878659f0ef9e7091b3ecd672c4fa9
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2025-01-11 15:29:49 +0900

    [feat] #13: 신고 - 털색 컬러 타입 FurColorType.kt 추가

---
+++ app/src/main/java/com/example/findu/presentation/type/report/FurColorType.kt
@@ -0,0 +1,15 @@
+package com.example.findu.presentation.type.report
+
+enum class FurColorType(
+    val color: String
+) {
+    BLACK("검은색"),
+    WHITE("하얀색"),
+    GRAY("회색"),
+    YELLOW("노란색"),
+    BROWN("갈색"),
+    RED("적색"),
+    SPOTTED("점박이"),
+    STRIPED("줄무늬"),
+    OTHER("기타")
+}
