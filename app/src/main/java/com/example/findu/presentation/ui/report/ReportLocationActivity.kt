e3171fddf107d02b1fa998d190b50ea5b5b8fdc9
IKSEONGKU <127182222+IKSEONGKU@users.noreply.github.com>
2025-01-20 23:42:16 +0900

    [feat] #21: Web View 로딩하는 함수 구현

--- app/src/main/java/com/example/findu/presentation/ui/report/ReportLocationActivity.kt
+++ app/src/main/java/com/example/findu/presentation/ui/report/ReportLocationActivity.kt
@@ -1,21 +1,60 @@
 package com.example.findu.presentation.ui.report
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.webkit.JavascriptInterface
+import android.webkit.WebView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.example.findu.R
+import androidx.webkit.WebViewAssetLoader
+import androidx.webkit.WebViewAssetLoader.AssetsPathHandler
+import androidx.webkit.WebViewClientCompat
+import com.example.findu.databinding.ActivityReportLocationBinding
 
 class ReportLocationActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityReportLocationBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_report_location)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(binding.root)
+
+        initWebView()
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun initWebView() {
+        val webView = binding.wvReportLocation
+        val assetLoader = WebViewAssetLoader.Builder()
+            .addPathHandler(ASSETS, AssetsPathHandler(this))
+            .build()
+        webView.settings.javaScriptEnabled = true
+        webView.webViewClient = LocalContentWebViewClient(assetLoader)
+
+        // JavaScript 인터페이스 추가
+        webView.addJavascriptInterface(BridgeInterface(), ANDROID)
+
+        // HTML 파일 로드
+        webView.loadUrl(ASSETS_URL)
+    }
+
+    inner class LocalContentWebViewClient(private val assetLoader: WebViewAssetLoader) :
+        WebViewClientCompat() {
+        override fun onPageFinished(view: WebView?, url: String?) {
+            view?.loadUrl(DAUM_POST_CODE)
         }
+    }
+
+    inner class BridgeInterface() {
+        @JavascriptInterface
+        fun processData(postData: String) {
+            finish()
+        }
+    }
+
+    companion object {
+        private const val ASSETS = "/assets/"
+        private const val ANDROID = "Android"
+        private const val ASSETS_URL = "https://appassets.androidplatform.net/assets/index.html"
+        private const val DAUM_POST_CODE = "javascript:sample2_execDaumPostcode();"
     }
 }
