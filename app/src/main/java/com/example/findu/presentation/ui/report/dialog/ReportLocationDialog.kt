1deb674a45a549eae4146eda1dd6fae135581f44
IKSEONGKU <127182222+IKSEONGKU@users.noreply.github.com>
2025-01-20 21:23:35 +0900

    [feat] #21: naverMap 설정 Callback 함수 추가

--- app/src/main/java/com/example/findu/presentation/ui/report/dialog/ReportLocationDialog.kt
+++ app/src/main/java/com/example/findu/presentation/ui/report/dialog/ReportLocationDialog.kt
@@ -1,20 +1,27 @@
 package com.example.findu.presentation.ui.report.dialog
 
+import android.app.Activity
 import android.app.Dialog
 import android.content.Context
 import android.graphics.Color
-import android.graphics.Paint
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.SearchEvent
 import com.example.findu.databinding.DialogReportLocationBinding
+import com.naver.maps.map.LocationTrackingMode
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.OnMapReadyCallback
+import com.naver.maps.map.util.FusedLocationSource
 
 class ReportLocationDialog(
     context: Context
-) : Dialog(context) {
+) : Dialog(context), OnMapReadyCallback {
 
     private var _binding: DialogReportLocationBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var naverMap: NaverMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,14 +31,28 @@ class ReportLocationDialog(
 
         setContentView(binding.root)
 
-        binding.tvReportLocationDialogLocation.paintFlags = Paint.UNDERLINE_TEXT_FLAG
-
+        initMapView()
         initListener()
+    }
+
+    private fun initMapView() {
+
+        val mapView = binding.mvReportLocationDialogMap
+        mapView.getMapAsync(this)
     }
 
     private fun initListener() {
         binding.ivReportLocationDialogLocation.setOnClickListener { }
 
         binding.btnReportLocationDialogSet.setOnClickListener { }
+    }
+
+    override fun onMapReady(p0: NaverMap) {
+        naverMap = p0
+        with(naverMap.uiSettings) {
+            isZoomControlEnabled = false
+            isScaleBarEnabled = false
+            isLocationButtonEnabled = true
+        }
     }
 }
