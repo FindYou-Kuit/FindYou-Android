08abb48ca7edc7fa67953c449b8bf8cd15727694
soheekim <y04828@naver.com>
2025-02-02 10:32:12 -0500

    [refactor] #20: 조회필터 - 변수명 리팩

--- app/src/main/java/com/example/findu/presentation/ui/search/SearchFilterBottomSheet.kt
+++ app/src/main/java/com/example/findu/presentation/ui/search/SearchFilterBottomSheet.kt
@@ -2,9 +2,7 @@ package com.example.findu.presentation.ui.search
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.util.Log
 import android.view.*
-import android.widget.ArrayAdapter
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.archit.calendardaterangepicker.customviews.CalendarListener
@@ -22,13 +20,13 @@ class SearchFilterBottomSheet : BottomSheetDialogFragment() {
     lateinit var binding: FragmentSearchFilterBottomSheetBinding
     private lateinit var breedAdapter: SearchFilterBreedRVAdapter
     private lateinit var cityAdapter: SearchFilterLocationRVAdapter
-    private lateinit var guAdapter: SearchFilterLocationRVAdapter
+    private lateinit var districtAdapter: SearchFilterLocationRVAdapter
 
-    private val breeds =
+    private val breedList =
         listOf("리트리버", "말티즈", "불독", "사모예드", "시츄", "요크셔 테리어", "치와와", "포메라니안", "웰시코기")
     private val selectedBreeds = mutableListOf<String>()
 
-    private val locations =
+    private val cityList =
         listOf(
             "전체",
             "서울특별시",
@@ -47,10 +45,10 @@ class SearchFilterBottomSheet : BottomSheetDialogFragment() {
             "경상남도",
             "제주특별자치도",
         )
-    private var city: String? = null
+    private var selectedCity: String? = null
 
-    private val gus = listOf("전체", "강남구", "강동구", "강북구", "강서구", "관악구", "광진구", "구로구", "금천구", "노원구")
-    private var gu: String? = null
+    private val districtsList = listOf("전체", "강남구", "강동구", "강북구", "강서구", "관악구", "광진구", "구로구", "금천구", "노원구")
+    private var selectedDistrict: String? = null
 
     private val locationMap = LocationData.locationMap
 
@@ -59,90 +57,85 @@ class SearchFilterBottomSheet : BottomSheetDialogFragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentSearchFilterBottomSheetBinding.inflate(inflater, container, false)
-        binding.ivSearchFilterCloseBtn.setOnClickListener {
-            dismiss()
-        }
-        binding.btnSearchFilterConfirm.setOnClickListener {
-            applyFilters()
-        }
+        initListeners()
         setCalender()
         setBreedSelector()
         setLocationSelector()
         return binding.root
     }
-
+    private fun initListeners() {
+        binding.ivSearchFilterCloseBtn.setOnClickListener { dismiss() }
+        binding.btnSearchFilterConfirm.setOnClickListener { applyFilters() }
+    }
 
     private fun applyFilters() {
-        val filters = mutableListOf<String>()
+        val filterList = mutableListOf<String>()
 
-        val date = binding.tvSearchFilterDateInput.text.toString()
-        if (date.isNotEmpty() && date != getString(R.string.search_filter_date_input)) {
-            filters.add(date)
+        val selectedDate = binding.tvSearchFilterDateInput.text.toString()
+        if (selectedDate.isNotEmpty() && selectedDate != getString(R.string.search_filter_date_input)) {
+            filterList.add(selectedDate)
         }
 
-        val species = listOf(
+        val selectedSpecies = listOf(
             binding.rbDog,
             binding.rbCat,
             binding.rbEtc
         ).find { it.isChecked }?.text.toString()
 
-        if (species.isNotBlank()) {
-            filters.add(species)
+        if (selectedSpecies.isNotBlank()) {
+            filterList.add(selectedSpecies)
         }
-        if (filters.contains("null"))
-            filters.remove("null")
+        if (filterList.contains("null"))
+            filterList.remove("null")
 
-        val breed = selectedBreeds.toList()
         if (selectedBreeds.isNotEmpty()) {
-            filters.addAll(breed)
+            filterList.addAll(selectedBreeds)
 
         }
 
+        selectedCity?.let { if (it.isNotEmpty()) filterList.add(it) }
+        selectedDistrict?.let { if (it.isNotEmpty()) filterList.add(it) }
 
-        city?.let { if (it.isNotEmpty()) filters.add(it) }
-        gu?.let { if (it.isNotEmpty()) filters.add(it) }
-
-        if (filters.isEmpty()) {
+        if (filterList.isEmpty()) {
             dismiss()
             return
         }
 
         val bundle = Bundle().apply {
-            putStringArrayList("selectedFilters", ArrayList(filters))
+            putStringArrayList("selectedFilters", ArrayList(filterList))
         }
         parentFragmentManager.setFragmentResult("filterResults", bundle)
 
         dismiss()
     }
 
-
     override fun getTheme(): Int = R.style.searchFilterBottomSheetDialogTheme
 
     private fun setLocationSelector() {
         binding.actvSearchFilterGu.isEnabled = false
 
-        cityAdapter = SearchFilterLocationRVAdapter(locations, city) { newLocation ->
-            city = newLocation
+        cityAdapter = SearchFilterLocationRVAdapter(cityList, selectedCity) { newCity ->
+            selectedCity = newCity
             updateSelectedLocation()
 
-            if (newLocation == "전체") {
+            if (newCity == "전체") {
                 binding.actvSearchFilterGu.isEnabled = false
                 binding.actvSearchFilterGu.setText("")
-                gu = null
+                selectedDistrict = null
             } else {
                 binding.actvSearchFilterGu.isEnabled = true
-                val guList = locationMap[newLocation] ?: listOf("")
-                guAdapter = SearchFilterLocationRVAdapter(guList, gu) { newGu ->
-                    gu = newGu
+                val districtList = locationMap[newCity] ?: listOf("")
+                districtAdapter = SearchFilterLocationRVAdapter(districtList, selectedDistrict) { newDistrict ->
+                    selectedDistrict = newDistrict
                     updateSelectedLocation()
                 }
-                binding.rvSearchFilterGu.adapter = guAdapter
+                binding.rvSearchFilterGu.adapter = districtAdapter
                 binding.actvSearchFilterGu.setText("")
             }
 
         }
-        guAdapter = SearchFilterLocationRVAdapter(gus, gu) { newGu ->
-            gu = newGu
+        districtAdapter = SearchFilterLocationRVAdapter(districtsList, selectedDistrict) { newDistrict ->
+            selectedDistrict = newDistrict
             updateSelectedLocation()
         }
 
@@ -150,62 +143,59 @@ class SearchFilterBottomSheet : BottomSheetDialogFragment() {
         binding.rvSearchFilterCity.adapter = cityAdapter
 
         binding.actvSearchFilterCity.setOnClickListener {
-            if (binding.rvSearchFilterCity.visibility == View.GONE) {
-                binding.rvSearchFilterCity.visibility = View.VISIBLE
-                binding.actvSearchFilterCity.setBackgroundResource(R.drawable.bg_search_radius_8_up)
-            } else {
-                binding.rvSearchFilterCity.visibility = View.GONE
-                binding.actvSearchFilterCity.setBackgroundResource(R.drawable.bg_search_radius_8)
-            }
+            toggleRecyclerViewVisibility(
+                binding.rvSearchFilterCity,
+                binding.actvSearchFilterCity
+            )
         }
         binding.rvSearchFilterGu.layoutManager = LinearLayoutManager(requireContext())
-        binding.rvSearchFilterGu.adapter = guAdapter
+        binding.rvSearchFilterGu.adapter = districtAdapter
 
         binding.actvSearchFilterGu.setOnClickListener {
-            if (binding.actvSearchFilterGu.isEnabled) {
-                if (binding.rvSearchFilterGu.visibility == View.GONE) {
-                    binding.rvSearchFilterGu.visibility = View.VISIBLE
-                    binding.actvSearchFilterGu.setBackgroundResource(R.drawable.bg_search_radius_8_up)
-                } else {
-                    binding.rvSearchFilterGu.visibility = View.GONE
-                    binding.actvSearchFilterGu.setBackgroundResource(R.drawable.bg_search_radius_8)
-                }
-            }
+            toggleRecyclerViewVisibility(
+                binding.rvSearchFilterGu,
+                binding.actvSearchFilterGu
+            )
+        }
+    }
+    private fun toggleRecyclerViewVisibility(recyclerView: View, triggerView: View) {
+        if (recyclerView.visibility == View.GONE) {
+            recyclerView.visibility = View.VISIBLE
+            triggerView.setBackgroundResource(R.drawable.bg_search_radius_8_up)
+        } else {
+            recyclerView.visibility = View.GONE
+            triggerView.setBackgroundResource(R.drawable.bg_search_radius_8)
         }
     }
 
     private fun updateSelectedLocation() {
-        binding.actvSearchFilterCity.setText(city ?: "")
+        binding.actvSearchFilterCity.setText(selectedCity ?: "")
         binding.rvSearchFilterCity.visibility = View.GONE
         binding.actvSearchFilterCity.setBackgroundResource(R.drawable.bg_search_radius_8)
 
-        binding.actvSearchFilterGu.setText(gu ?: "")
+        binding.actvSearchFilterGu.setText(selectedDistrict ?: "")
         binding.rvSearchFilterGu.visibility = View.GONE
         binding.actvSearchFilterGu.setBackgroundResource(R.drawable.bg_search_radius_8)
     }
 
     private fun setBreedSelector() {
         breedAdapter =
-            SearchFilterBreedRVAdapter(breeds, selectedBreeds) { updateSelectedBreeds() }
+            SearchFilterBreedRVAdapter(breedList, selectedBreeds) { updateSelectedBreeds() }
         binding.rvSearchFilterBreeds.layoutManager = LinearLayoutManager(requireContext())
         binding.rvSearchFilterBreeds.adapter = breedAdapter
 
         binding.actvSearchFilterBreed.setOnClickListener {
-            if (binding.rvSearchFilterBreeds.visibility == View.GONE) {
-                binding.rvSearchFilterBreeds.visibility = View.VISIBLE
-                binding.actvSearchFilterBreed.setBackgroundResource(R.drawable.bg_search_radius_8_up)
-
-            } else {
-                binding.rvSearchFilterBreeds.visibility = View.GONE
-                binding.actvSearchFilterBreed.setBackgroundResource(R.drawable.bg_search_radius_8)
-            }
+            toggleRecyclerViewVisibility(
+                binding.rvSearchFilterBreeds,
+                binding.actvSearchFilterBreed
+            )
         }
     }
+
 
     private fun updateSelectedBreeds() {
         binding.actvSearchFilterBreed.setText(selectedBreeds.joinToString(", "))
     }
-
 
     private fun setCalender() {
         binding.tvSearchFilterDateInput.setOnClickListener {
