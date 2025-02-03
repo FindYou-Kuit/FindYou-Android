f3f65e15c47ab41c4157cceaeacd82aabfbead49
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2025-02-03 15:31:09 +0900

    [feat] #28: cameraX 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -98,4 +98,11 @@ dependencies {
     
     // Calender
     implementation("io.github.architshah248.calendar:awesome-calendar:2.0.0")
+
+    // cameraX
+    implementation(libs.androidx.camera.core)
+    implementation(libs.androidx.camera.camera2)
+    implementation(libs.androidx.camera.lifecycle)
+    implementation(libs.androidx.camera.view)
+    implementation(libs.androidx.camera.extensions)
 }
