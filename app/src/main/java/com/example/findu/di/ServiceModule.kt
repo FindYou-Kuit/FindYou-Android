070fafbf763f1f53e618cdecacc5e07b52cc4145
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2025-02-06 02:35:22 +0900

    [feat] #31: DI - Gpt 용 Retrofit 을 생성하는 의존성 주입 함수 구현

--- app/src/main/java/com/example/findu/di/ServiceModule.kt
+++ app/src/main/java/com/example/findu/di/ServiceModule.kt
@@ -2,10 +2,14 @@ package com.example.findu.di
 
 import com.example.findu.data.dataremote.service.DummyService
 import com.example.findu.data.dataremote.service.GptService
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import javax.inject.Singleton
 
@@ -19,7 +23,19 @@ object ServiceModule {
 
     @Provides
     @Singleton
-    fun provideGptService(retrofit: Retrofit): GptService =
-        retrofit.create(GptService::class.java)
+    fun provideGptService(
+        okHttpClient: OkHttpClient,
+        json: Json
+    ): GptService {
+        val gptRetrofit = Retrofit.Builder()
+            .baseUrl("https://api.openai.com/v1/")
+            .client(okHttpClient)
+            .addConverterFactory(
+                json.asConverterFactory(requireNotNull("application/json".toMediaTypeOrNull()))
+            )
+            .build()
+
+        return gptRetrofit.create(GptService::class.java)
+    }
 
 }
