b770cb49bd9422661722d35ecf5574be49842763
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2025-02-06 02:34:12 +0900

    [feat] #31: data - Gpt 리스폰스를 도메인 모델로 변환하는 Mapper 구현

--- app/src/main/java/com/example/findu/data/mapper/toDomain/GptResponseDtoMapper.kt
+++ app/src/main/java/com/example/findu/data/mapper/toDomain/GptResponseDtoMapper.kt
@@ -1,11 +1,50 @@
 package com.example.findu.data.mapper.toDomain
 
 import com.example.findu.data.dataremote.model.response.GptResponseDto
+import com.example.findu.domain.model.report.Breed
+import com.example.findu.domain.model.report.FurColorType
 import com.example.findu.domain.model.report.GptData
+import com.example.findu.domain.model.report.SpeciesType
 import kotlinx.serialization.json.Json
 
 fun GptResponseDto.toDomain(): GptData {
     return this.choices.firstOrNull()?.message?.content?.let {
-        Json.decodeFromString<GptData>(it)
+        val split = it.split(",")
+
+        GptData(
+            breed = Breed(split[0]),
+            species = when (split[1]) {
+                "강아지" -> SpeciesType.DOG
+                "고양이" -> SpeciesType.CAT
+                else -> SpeciesType.ETC
+            },
+            furColors = split.subList(2, split.size).map {
+                when (it) {
+                    "검은색" -> FurColorType.BLACK
+                    "하얀색" -> FurColorType.WHITE
+                    "갈색" -> FurColorType.BROWN
+                    "회색" -> FurColorType.GRAY
+                    "적색" -> FurColorType.RED
+                    "노란색" -> FurColorType.YELLOW
+                    "점박이" -> FurColorType.SPOTTED
+                    "줄무늬" -> FurColorType.STRIPED
+                    else -> FurColorType.OTHER
+                }
+            }
+//            split.forEachIndexed { index, color ->
+//                if (index < 2) return@forEachIndexed
+//                when (color) {
+//                    "검은색" -> FurColorType.BLACK
+//                    "하얀색" -> FurColorType.WHITE
+//                    "갈색" -> FurColorType.BROWN
+//                    "회색" -> FurColorType.GRAY
+//                    "적색" -> FurColorType.RED
+//                    "노란색" -> FurColorType.YELLOW
+//                    "점박이" -> FurColorType.SPOTTED
+//                    "줄무늬" -> FurColorType.STRIPED
+//                    else -> FurColorType.OTHER
+//                }
+//            }.toList()
+        )
     } ?: GptData()
 }
--- app/src/main/java/com/example/findu/data/repositoryimpl/ReportRepositoryImpl.kt
+++ app/src/main/java/com/example/findu/data/repositoryimpl/ReportRepositoryImpl.kt
@@ -1,25 +1,21 @@
 package com.example.findu.data.repositoryimpl
 
-import android.content.Context
-import android.net.Uri
 import com.example.findu.data.dataremote.datasource.GptRemoteDataSource
 import com.example.findu.data.dataremote.model.request.GptRequestDto
+import com.example.findu.data.dataremote.model.request.GptRequestDto.Companion.imageContent
+import com.example.findu.data.dataremote.model.request.ImageUrl
 import com.example.findu.data.mapper.toDomain.toDomain
-import com.example.findu.presentation.util.UriUtil.uriToBase64
 import com.example.findu.domain.model.report.GptData
 import com.example.findu.domain.repository.report.ReportRepository
-import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 
 class ReportRepositoryImpl @Inject constructor(
-    private val gptRemoteDataSource: GptRemoteDataSource,
-    @ApplicationContext private val context: Context
+    private val gptRemoteDataSource: GptRemoteDataSource
 ) : ReportRepository {
     override suspend fun postImageAnalysis(encodeString: String): Result<GptData> {
         val request = GptRequestDto().apply {
-            GptRequestDto.imageContent.imageUrl = encodeString
+            imageContent.imageUrl = ImageUrl(url = encodeString)
         }
-
         return runCatching { gptRemoteDataSource.postImagePrompt(request).toDomain() }
     }
 }
