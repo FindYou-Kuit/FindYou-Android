3fe8dcb702991d0df8d1d33ed2863630faba1d8a
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2025-02-05 21:22:08 +0900

    [feat] #31: presentation - viewModel 에서 Usecase 함수 호출

--- app/src/main/java/com/example/findu/presentation/ui/report/ReportViewModel.kt
+++ app/src/main/java/com/example/findu/presentation/ui/report/ReportViewModel.kt
@@ -1,12 +1,42 @@
 package com.example.findu.presentation.ui.report
 
+import android.content.Context
+import android.net.Uri
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.findu.domain.model.report.GptData
 import com.example.findu.domain.usecase.DummyUseCase
+import com.example.findu.domain.usecase.report.AnalysisImageWithGptUseCase
+import com.example.findu.presentation.util.UriUtil.uriToBase64
 import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class ReportViewModel @Inject constructor(
-    private val dummyDataUseCase: DummyUseCase
+    private val analysisImageWithGptUseCase: AnalysisImageWithGptUseCase,
+    @ApplicationContext private val context: Context
 ) : ViewModel() {
+    private val _gptData: MutableStateFlow<GptData> = MutableStateFlow(GptData())
+    val gptData = _gptData
+
+    private val _errorMessage: MutableStateFlow<String> = MutableStateFlow("")
+    val errorMessage = _errorMessage
+
+    fun getGptData(imageUri: Uri) {
+        viewModelScope.launch {
+            val encodeString = imageUri.uriToBase64(context) ?: ""
+
+            analysisImageWithGptUseCase(encodeString).fold(
+                onSuccess = {
+                    _gptData.value = it
+                },
+                onFailure = {
+                    _errorMessage.value = it.message.toString()
+
+                })
+        }
+    }
 }
