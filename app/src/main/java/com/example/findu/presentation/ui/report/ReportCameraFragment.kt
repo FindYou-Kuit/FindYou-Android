0c0f7f0c157ff1161c50595dbd12cad564bdef78
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2025-02-05 14:04:19 +0900

    [feat] #28: 카메라를 사용하는 프래그먼트 구현

--- app/src/main/java/com/example/findu/presentation/ui/report/ReportCameraFragment.kt
+++ app/src/main/java/com/example/findu/presentation/ui/report/ReportCameraFragment.kt
@@ -1,11 +1,27 @@
 package com.example.findu.presentation.ui.report
 
+import android.Manifest
+import android.app.Activity
+import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.findu.databinding.FragmentReportCameraBinding
+import com.example.findu.presentation.util.PermissionUtils
+import com.example.findu.presentation.util.PermissionUtils.hasCameraPermission
+import com.example.findu.presentation.util.PermissionUtils.requestPermission
 
 class ReportCameraFragment : Fragment() {
 
@@ -18,9 +34,50 @@ class ReportCameraFragment : Fragment() {
     ): View {
         _binding = FragmentReportCameraBinding.inflate(inflater, container, false)
 
-
+        if(hasCameraPermission(requireContext())) {
+            startCamera()
+        } else {
+            requestPermission(requireContext())
+        }
 
         return binding.root
     }
 
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(requireActivity())
+
+        cameraProviderFuture.addListener({
+            // Used to bind the lifecycle of cameras to the lifecycle owner
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            // Preview
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(binding.pvReportCameraViewFinder.surfaceProvider)
+                }
+
+            val imageCapture = ImageCapture.Builder()
+                .build()
+
+            // Select back camera as a default
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+                // Unbind use cases before rebinding
+                cameraProvider.unbindAll()
+
+                // Bind use cases to camera
+                cameraProvider.bindToLifecycle(
+                    this, cameraSelector, preview, imageCapture
+                )
+
+            } catch (exc: Exception) {
+            }
+
+        }, ContextCompat.getMainExecutor(requireActivity()))
+    }
+
+
 }
+
