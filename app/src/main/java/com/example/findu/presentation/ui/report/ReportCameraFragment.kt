4ff2cd8732b1e4322a9f09a5d484212999ae50fb
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2025-02-07 20:24:49 +0900

    [feat] #31: fragment 에서 카메라 권한을 체크하고 요청하는 기능 구현

--- app/src/main/java/com/example/findu/presentation/ui/report/ReportCameraFragment.kt
+++ app/src/main/java/com/example/findu/presentation/ui/report/ReportCameraFragment.kt
@@ -1,32 +1,36 @@
 package com.example.findu.presentation.ui.report
 
 import android.Manifest
-import android.app.Activity
-import android.content.Context
-import android.content.pm.PackageManager
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageCapture
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
-import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.navigation.fragment.findNavController
 import com.example.findu.databinding.FragmentReportCameraBinding
-import com.example.findu.presentation.util.PermissionUtils
 import com.example.findu.presentation.util.PermissionUtils.hasCameraPermission
-import com.example.findu.presentation.util.PermissionUtils.requestPermission
 
 class ReportCameraFragment : Fragment() {
 
     private var _binding: FragmentReportCameraBinding? = null
     private val binding get() = _binding!!
+    private val requestPermissionLauncher: ActivityResultLauncher<String> by lazy {
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (isGranted) {
+                startCamera()
+            } else {
+                findNavController().popBackStack()
+            }
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -34,11 +38,12 @@ class ReportCameraFragment : Fragment() {
     ): View {
         _binding = FragmentReportCameraBinding.inflate(inflater, container, false)
 
-        if(hasCameraPermission(requireContext())) {
+        if (hasCameraPermission(requireContext())) {
             startCamera()
         } else {
-            requestPermission(requireContext())
+            requestPermissionLauncher.launch(Manifest.permission.CAMERA)
         }
+
 
         return binding.root
     }
@@ -73,6 +78,9 @@ class ReportCameraFragment : Fragment() {
                 )
 
             } catch (exc: Exception) {
+                Toast.makeText(requireContext(), "Failed to Start Camera", Toast.LENGTH_SHORT)
+                    .show()
+                findNavController().popBackStack()
             }
 
         }, ContextCompat.getMainExecutor(requireActivity()))
