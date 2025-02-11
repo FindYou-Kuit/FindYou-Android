31e6468df5d96aa23357c88a93e35d482fb189e5
IKSEONGKU <127182222+IKSEONGKU@users.noreply.github.com>
2025-02-11 15:41:31 +0900

    [feat] #25: presentation - ReportViewModel 에 품종 정보를 불러오는 기능 추가

--- app/src/main/java/com/example/findu/presentation/ui/report/ReportViewModel.kt
+++ app/src/main/java/com/example/findu/presentation/ui/report/ReportViewModel.kt
@@ -1,12 +1,41 @@
 package com.example.findu.presentation.ui.report
 
 import androidx.lifecycle.ViewModel
-import com.example.findu.domain.usecase.DummyUseCase
+import androidx.lifecycle.viewModelScope
+import com.example.findu.domain.model.breed.BreedData
+import com.example.findu.domain.usecase.GetBreedDataUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class ReportViewModel @Inject constructor(
-    private val dummyDataUseCase: DummyUseCase
+    private val getBreedDataUseCase: GetBreedDataUseCase
 ) : ViewModel() {
+    private val _breedData = MutableStateFlow(BreedData())
+    val breedData: StateFlow<BreedData> = _breedData
+
+    private val _errorMessage = MutableStateFlow<String?>(null)  // 오류 메시지 관리
+    val errorMessage = _errorMessage.asStateFlow()
+
+    init {
+        getBreedData()
+    }
+
+    private fun getBreedData() {
+        viewModelScope.launch {
+            getBreedDataUseCase().fold(
+                onSuccess = { data ->
+                    _breedData.value = data
+                },
+                onFailure = { error ->
+                    _errorMessage.value = error.message ?: "데이터를 불러오는 중 오류가 발생했습니다."
+                }
+            )
+        }
+    }
+
 }
