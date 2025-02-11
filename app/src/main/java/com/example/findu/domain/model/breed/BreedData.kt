697cf796e61a2fd17424cea3269b3c812346f729
IKSEONGKU <127182222+IKSEONGKU@users.noreply.github.com>
2025-02-11 15:37:45 +0900

    [feat] #25: domain - BreedData 에 Dog,Cat,Etc 에 해당하는 리스트 추가

--- app/src/main/java/com/example/findu/domain/model/breed/BreedData.kt
+++ app/src/main/java/com/example/findu/domain/model/breed/BreedData.kt
@@ -1,6 +1,24 @@
 package com.example.findu.domain.model.breed
 
 data class BreedData(
+    val dogList : List<DogData> = emptyList(),
+    val catList : List<CatData> = emptyList(),
+    val etcList : List<EtcData> = emptyList()
+)
+
+data class CatData(
+    val breedId: Int,
+    val breedName: String,
+    val species: SpeciesType
+)
+
+data class DogData(
+    val breedId: Int,
+    val breedName: String,
+    val species: SpeciesType
+)
+
+data class EtcData(
     val breedId: Int,
     val breedName: String,
     val species: SpeciesType
