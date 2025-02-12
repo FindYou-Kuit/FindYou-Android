d852db260f5fe40419ee70ab08c2ac9777e1d13b
soheekim <y04828@naver.com>
2025-02-13 02:39:42 +0900

    [refactor] #36 : domain data 내용 수정

--- app/src/main/java/com/example/findu/domain/model/search/SearchData.kt
+++ app/src/main/java/com/example/findu/domain/model/search/SearchData.kt
@@ -3,10 +3,14 @@ package com.example.findu.domain.model.search
 import java.io.Serializable
 
 data class SearchData(
-    val image : Int,
-    val name : String,
-    val date : String,
-    val address : String,
-    var isBookmark : Boolean,
-    var status : SearchStatus,
+    val cardId: Long,
+    val thumbnailImageUrl: String,
+    val title: String,
+    var tag: SearchStatus,
+    val date: String,
+    val location: String,
+    val interest: Boolean,
+    val lastProtectId: Long,
+    val lastReportId: Long,
+    val isLast: Boolean
 ) : Serializable
