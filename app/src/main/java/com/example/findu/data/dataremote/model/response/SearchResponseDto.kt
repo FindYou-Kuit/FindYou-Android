5bca87dc1ba45f6db1279938535b1ce9b852276b
soheekim <y04828@naver.com>
2025-02-13 01:48:44 +0900

    [feat] #36 : Dto정의

---
+++ app/src/main/java/com/example/findu/data/dataremote/model/response/SearchResponseDto.kt
@@ -0,0 +1,28 @@
+package com.example.findu.data.dataremote.model.response
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class SearchResponseDto(
+    @SerialName("cardId")
+    val cardId: Int,
+    @SerialName("thumbnailImageUrl")
+    val thumbnailImageUrl: String,
+    @SerialName("title")
+    val title: String,
+    @SerialName("tag")
+    val tag: String,
+    @SerialName("date")
+    val date: String,
+    @SerialName("location")
+    val location: String,
+    @SerialName("interest")
+    val interest: Boolean,
+    @SerialName("lastProtectId")
+    val lastProtectId: Long,
+    @SerialName("lastReportId")
+    val lastReportId: Long,
+    @SerialName("isLast")
+    val isLast: Boolean,
+)
