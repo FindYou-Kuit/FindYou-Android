4658f158168e929cffac2f867f13185d2328782e
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2025-02-14 00:29:34 +0900

    [refactor] #31: return 키워드 대신 Expression Body 사용 및 String 값 대신 Enum value 사용

--- app/src/main/java/com/example/findu/data/mapper/toDomain/GptResponseDtoMapper.kt
+++ app/src/main/java/com/example/findu/data/mapper/toDomain/GptResponseDtoMapper.kt
@@ -1,36 +1,34 @@
-package com.example.findu.data.mapper.toDomain
+package com.example.findu.data.mapper.todomain
 
 import com.example.findu.data.dataremote.model.response.GptResponseDto
 import com.example.findu.domain.model.report.Breed
 import com.example.findu.domain.model.report.FurColorType
 import com.example.findu.domain.model.report.GptData
 import com.example.findu.domain.model.report.SpeciesType
-import kotlinx.serialization.json.Json
 
-fun GptResponseDto.toDomain(): GptData {
-    return this.choices.firstOrNull()?.message?.content?.let {
-        val split = it.split(",")
+fun GptResponseDto.toDomain(): GptData =
+    this.choices.firstOrNull()?.message?.content?.let { content ->
+        val parsedData = content.split(",")
 
         GptData(
-            breed = Breed(split[0]),
-            species = when (split[1]) {
-                "강아지" -> SpeciesType.DOG
-                "고양이" -> SpeciesType.CAT
+            breed = Breed(parsedData[0]),
+            species = when (parsedData[1]) {
+                SpeciesType.DOG.species -> SpeciesType.DOG
+                SpeciesType.CAT.species -> SpeciesType.CAT
                 else -> SpeciesType.ETC
             },
-            furColors = split.subList(2, split.size).map {
-                when (it) {
-                    "검은색" -> FurColorType.BLACK
-                    "하얀색" -> FurColorType.WHITE
-                    "갈색" -> FurColorType.BROWN
-                    "회색" -> FurColorType.GRAY
-                    "적색" -> FurColorType.RED
-                    "노란색" -> FurColorType.YELLOW
-                    "점박이" -> FurColorType.SPOTTED
-                    "줄무늬" -> FurColorType.STRIPED
+            furColors = parsedData.drop(2).map { color ->
+                when (color) {
+                    FurColorType.BLACK.color -> FurColorType.BLACK
+                    FurColorType.WHITE.color -> FurColorType.WHITE
+                    FurColorType.BROWN.color -> FurColorType.BROWN
+                    FurColorType.GRAY.color -> FurColorType.GRAY
+                    FurColorType.RED.color -> FurColorType.RED
+                    FurColorType.YELLOW.color -> FurColorType.YELLOW
+                    FurColorType.SPOTTED.color -> FurColorType.SPOTTED
+                    FurColorType.STRIPED.color -> FurColorType.STRIPED
                     else -> FurColorType.OTHER
                 }
             }
         )
     } ?: GptData()
-}
