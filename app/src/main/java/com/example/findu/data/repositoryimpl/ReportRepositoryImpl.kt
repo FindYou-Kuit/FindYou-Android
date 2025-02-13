723a845740ccd956328b380774d0bc97817527c5
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2025-02-14 00:28:21 +0900

    [refactor] #31: return 키워드 대신 Expression Body 사용 및 String 값 대신 Enum value 사용

--- app/src/main/java/com/example/findu/data/repositoryimpl/ReportRepositoryImpl.kt
+++ app/src/main/java/com/example/findu/data/repositoryimpl/ReportRepositoryImpl.kt
@@ -4,7 +4,7 @@ import com.example.findu.data.dataremote.datasource.GptRemoteDataSource
 import com.example.findu.data.dataremote.model.request.GptRequestDto
 import com.example.findu.data.dataremote.model.request.GptRequestDto.Companion.imageContent
 import com.example.findu.data.dataremote.model.request.ImageUrl
-import com.example.findu.data.mapper.toDomain.toDomain
+import com.example.findu.data.mapper.todomain.toDomain
 import com.example.findu.domain.model.report.GptData
 import com.example.findu.domain.repository.report.ReportRepository
 import javax.inject.Inject
