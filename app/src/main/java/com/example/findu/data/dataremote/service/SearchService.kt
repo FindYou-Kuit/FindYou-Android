61dbe82a968ecb1170521fba74e186614d187df0
soheekim <y04828@naver.com>
2025-02-16 17:17:06 +0900

    [refactor] : 조회 탭레이아웃 배경색 변경

--- app/src/main/java/com/example/findu/data/dataremote/service/SearchService.kt
+++ app/src/main/java/com/example/findu/data/dataremote/service/SearchService.kt
@@ -4,6 +4,7 @@ import com.example.findu.data.dataremote.model.base.BaseResponse
 import com.example.findu.data.dataremote.model.response.SearchResponseDto
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface SearchService {
