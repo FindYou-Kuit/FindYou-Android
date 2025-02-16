7b246fdc485432dd04f1ebe2616ec6b93fcec21e
soheekim <y04828@naver.com>
2025-02-17 02:23:57 +0900

    [refactor] #36 : 조회상세 - view model 생성

---
+++ app/src/main/java/com/example/findu/presentation/ui/search/viewmodel/DetailSearchViewModel.kt
@@ -0,0 +1,65 @@
+package com.example.findu.presentation.ui.search.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.findu.domain.model.search.SearchData
+import com.example.findu.domain.usecase.GetSearchUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class SearchViewModel @Inject constructor(
+    private val getSearchUseCase: GetSearchUseCase
+) : ViewModel() {
+
+    private val _searchData = MutableStateFlow<List<SearchData>?>(null)
+    val searchData = _searchData.asStateFlow()
+
+    private val _errorMessage = MutableStateFlow<String?>(null)
+    val errorMessage = _errorMessage.asStateFlow()
+
+    fun getSearchAllData(
+        lastProtectId: Long = Long.MAX_VALUE,
+        lastReportId: Long = Long.MAX_VALUE
+    ) {
+        viewModelScope.launch {
+            getSearchUseCase.getAllData(lastProtectId, lastReportId).fold(
+                onSuccess = { data ->
+                    _searchData.value = data
+                },
+                onFailure = { error ->
+                    _errorMessage.value = error.message ?: "데이터를 불러오는 중 오류가 발생했습니다."
+                }
+            )
+        }
+    }
+
+    fun getSearchReportData(lastReportId: Long = Long.MAX_VALUE) {
+        viewModelScope.launch {
+            getSearchUseCase.getReportData(lastReportId).fold(
+                onSuccess = { data ->
+                    _searchData.value = data
+                },
+                onFailure = { error ->
+                    _errorMessage.value = error.message ?: "신고 동물 데이터를 불러오는 중 오류가 발생했습니다."
+                }
+            )
+        }
+    }
+
+    fun getSearchProtectData(lastProtectId: Long = Long.MAX_VALUE) {
+        viewModelScope.launch {
+            getSearchUseCase.getProtectData(lastProtectId).fold(
+                onSuccess = { data ->
+                    _searchData.value = data
+                },
+                onFailure = { error ->
+                    _errorMessage.value = error.message ?: "신고 동물 데이터를 불러오는 중 오류가 발생했습니다."
+                }
+            )
+        }
+    }
+}
