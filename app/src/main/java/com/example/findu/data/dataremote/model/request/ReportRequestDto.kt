adc03d240707894b0a2c2c5fb68fcef2ba5d409f
IKSEONGKU <127182222+IKSEONGKU@users.noreply.github.com>
2025-02-17 16:40:56 +0900

    [feat] #37: data - 실종신고 Request Dto Mapper 생성

--- app/src/main/java/com/example/findu/data/dataremote/model/request/ReportRequestDto.kt
+++ app/src/main/java/com/example/findu/data/dataremote/model/request/ReportRequestDto.kt
@@ -7,7 +7,7 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class MissingReportRequestDto(
     @SerialName("imageUrls")
-    val imageUrls: List<String>,
+    val imageUrls: List<Int>,
     @SerialName("breed")
     val breed: Int,
     @SerialName("sex")
---
+++ app/src/main/java/com/example/findu/data/mapper/torequest/ReportRequestDtoMapper.kt
@@ -0,0 +1,16 @@
+package com.example.findu.data.mapper.torequest
+
+import com.example.findu.data.dataremote.model.request.MissingReportRequestDto
+import com.example.findu.domain.model.report.MissingReportData
+
+fun MissingReportData.toRequestDto() =
+    MissingReportRequestDto(
+        imageUrls = imageKeys,
+        breed = breedId,
+        sex = sex.value,
+        furColor = furColors.map { it.color },
+        location = location,
+        features = featureIds,
+        description = description,
+        missingDate = missingDate
+    )
