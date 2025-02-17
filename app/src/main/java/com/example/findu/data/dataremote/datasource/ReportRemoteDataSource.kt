86c4893875589bff2cac95774e870fa3ae2a5ab0
IKSEONGKU <127182222+IKSEONGKU@users.noreply.github.com>
2025-02-17 16:32:42 +0900

    [feat] #37: data 실종신고 DataSource 메소드 생성

--- app/src/main/java/com/example/findu/data/dataremote/datasource/ReportRemoteDataSource.kt
+++ app/src/main/java/com/example/findu/data/dataremote/datasource/ReportRemoteDataSource.kt
@@ -1,8 +1,12 @@
 package com.example.findu.data.dataremote.datasource
 
 import com.example.findu.data.dataremote.model.base.BaseResponse
+import com.example.findu.data.dataremote.model.base.NullableBaseResponse
+import com.example.findu.data.dataremote.model.request.MissingReportRequestDto
 import okhttp3.MultipartBody
 
 interface ReportRemoteDataSource {
     suspend fun uploadImages(files: List<MultipartBody.Part>): BaseResponse<List<Int>>
+
+    suspend fun postMissingReport(requestDto: MissingReportRequestDto) : NullableBaseResponse<Unit>
 }
--- app/src/main/java/com/example/findu/data/dataremote/datasourceimpl/ReportRemoteDataSourceImpl.kt
+++ app/src/main/java/com/example/findu/data/dataremote/datasourceimpl/ReportRemoteDataSourceImpl.kt
@@ -2,6 +2,8 @@ package com.example.findu.data.dataremote.datasourceimpl
 
 import com.example.findu.data.dataremote.datasource.ReportRemoteDataSource
 import com.example.findu.data.dataremote.model.base.BaseResponse
+import com.example.findu.data.dataremote.model.base.NullableBaseResponse
+import com.example.findu.data.dataremote.model.request.MissingReportRequestDto
 import com.example.findu.data.dataremote.service.ReportService
 import okhttp3.MultipartBody
 import javax.inject.Inject
@@ -11,4 +13,8 @@ class ReportRemoteDataSourceImpl @Inject constructor(
 ) : ReportRemoteDataSource {
     override suspend fun uploadImages(files: List<MultipartBody.Part>): BaseResponse<List<Int>> =
         service.uploadImages(files)
+
+    override suspend fun postMissingReport(requestDto: MissingReportRequestDto): NullableBaseResponse<Unit> =
+        service.postMissingReport(requestDto)
+
 }
