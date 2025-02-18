8acaf42abee44ff6a7e590b36dc76874b3ef3976
IKSEONGKU <127182222+IKSEONGKU@users.noreply.github.com>
2025-02-19 03:07:12 +0900

    [feat] #47: 최근 본 동물 datasource 구현

--- app/src/main/java/com/example/findu/data/dataremote/datasource/MyRemoteDataSource.kt
+++ app/src/main/java/com/example/findu/data/dataremote/datasource/MyRemoteDataSource.kt
@@ -3,6 +3,7 @@ package com.example.findu.data.dataremote.datasource
 import com.example.findu.data.dataremote.model.base.BaseResponse
 import com.example.findu.data.dataremote.model.response.my.MyInterestResponseDto
 import com.example.findu.data.dataremote.model.response.my.MyReportHistoryResponseDto
+import com.example.findu.data.dataremote.model.response.my.MyViewedAnimalsResponseDto
 
 interface MyRemoteDataSource {
     suspend fun getInterestAnimals(
@@ -13,4 +14,9 @@ interface MyRemoteDataSource {
     suspend fun getReportHistory(
         lastReportId: Long
     ): BaseResponse<MyReportHistoryResponseDto>
+
+    suspend fun getViewedAnimals(
+        lastReportId: Long,
+        lastProtectId: Long
+    ): BaseResponse<MyViewedAnimalsResponseDto>
 }
--- app/src/main/java/com/example/findu/data/dataremote/datasourceimpl/MyRemoteDataSourceImpl.kt
+++ app/src/main/java/com/example/findu/data/dataremote/datasourceimpl/MyRemoteDataSourceImpl.kt
@@ -4,6 +4,7 @@ import com.example.findu.data.dataremote.datasource.MyRemoteDataSource
 import com.example.findu.data.dataremote.model.base.BaseResponse
 import com.example.findu.data.dataremote.model.response.my.MyInterestResponseDto
 import com.example.findu.data.dataremote.model.response.my.MyReportHistoryResponseDto
+import com.example.findu.data.dataremote.model.response.my.MyViewedAnimalsResponseDto
 import com.example.findu.data.dataremote.service.MyService
 import javax.inject.Inject
 
@@ -18,4 +19,10 @@ class MyRemoteDataSourceImpl @Inject constructor(
 
     override suspend fun getReportHistory(lastReportId: Long): BaseResponse<MyReportHistoryResponseDto> =
         myService.getReportHistory(lastReportId)
+
+    override suspend fun getViewedAnimals(
+        lastReportId: Long,
+        lastProtectId: Long
+    ): BaseResponse<MyViewedAnimalsResponseDto> =
+        myService.getViewedAnimals(lastReportId, lastProtectId)
 }
