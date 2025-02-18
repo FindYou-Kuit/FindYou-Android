93136ae28ece42e29bec70ec517f2d5fa8b00fab
IKSEONGKU <127182222+IKSEONGKU@users.noreply.github.com>
2025-02-19 02:46:38 +0900

    [feat] #47: 신고 내역 조회 service 구현

---
+++ app/src/main/java/com/example/findu/data/dataremote/model/response/my/MyReportHistoryResponseDto.kt
@@ -0,0 +1,31 @@
+package com.example.findu.data.dataremote.model.response.my
+
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class MyReportHistoryResponseDto(
+    @SerialName("isLast")
+    val isLast: Boolean,
+    @SerialName("lastReportId")
+    val lastReportId: Int,
+    @SerialName("reports")
+    val reports: List<Report>
+) {
+    @Serializable
+    data class Report(
+        @SerialName("date")
+        val date: String,
+        @SerialName("location")
+        val location: String,
+        @SerialName("reportId")
+        val reportId: Int,
+        @SerialName("tag")
+        val tag: String,
+        @SerialName("thumbnailImageUrl")
+        val thumbnailImageUrl: String,
+        @SerialName("title")
+        val title: String
+    )
+}
--- app/src/main/java/com/example/findu/data/dataremote/service/MyService.kt
+++ app/src/main/java/com/example/findu/data/dataremote/service/MyService.kt
@@ -2,6 +2,7 @@ package com.example.findu.data.dataremote.service
 
 import com.example.findu.data.dataremote.model.base.BaseResponse
 import com.example.findu.data.dataremote.model.response.my.MyInterestResponseDto
+import com.example.findu.data.dataremote.model.response.my.MyReportHistoryResponseDto
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -11,4 +12,9 @@ interface MyService {
         @Query("lastInterestReportId") lastReportId: Long,
         @Query("lastInterestProtectId") lastProtectId: Long,
     ): BaseResponse<MyInterestResponseDto>
+
+    @GET("/api/v1/users/reports")
+    suspend fun getReportHistory(
+        @Query("lastReportId") lastReportId: Long,
+    ): BaseResponse<MyReportHistoryResponseDto>
 }
