1c261fd92a49919b109b354de51f730390ef1c94
IKSEONGKU <127182222+IKSEONGKU@users.noreply.github.com>
2025-02-19 02:47:46 +0900

    [feat] #47: 신고 내역 조회 datasource 구현

--- app/src/main/java/com/example/findu/data/dataremote/datasource/MyRemoteDataSource.kt
+++ app/src/main/java/com/example/findu/data/dataremote/datasource/MyRemoteDataSource.kt
@@ -2,10 +2,15 @@ package com.example.findu.data.dataremote.datasource
 
 import com.example.findu.data.dataremote.model.base.BaseResponse
 import com.example.findu.data.dataremote.model.response.my.MyInterestResponseDto
+import com.example.findu.data.dataremote.model.response.my.MyReportHistoryResponseDto
 
 interface MyRemoteDataSource {
     suspend fun getInterestAnimals(
         lastReportId: Long,
         lastProtectId: Long
     ): BaseResponse<MyInterestResponseDto>
+
+    suspend fun getReportHistory(
+        lastReportId: Long
+    ): BaseResponse<MyReportHistoryResponseDto>
 }
--- app/src/main/java/com/example/findu/data/dataremote/datasourceimpl/MyRemoteDataSourceImpl.kt
+++ app/src/main/java/com/example/findu/data/dataremote/datasourceimpl/MyRemoteDataSourceImpl.kt
@@ -3,6 +3,7 @@ package com.example.findu.data.dataremote.datasourceimpl
 import com.example.findu.data.dataremote.datasource.MyRemoteDataSource
 import com.example.findu.data.dataremote.model.base.BaseResponse
 import com.example.findu.data.dataremote.model.response.my.MyInterestResponseDto
+import com.example.findu.data.dataremote.model.response.my.MyReportHistoryResponseDto
 import com.example.findu.data.dataremote.service.MyService
 import javax.inject.Inject
 
@@ -14,4 +15,7 @@ class MyRemoteDataSourceImpl @Inject constructor(
         lastProtectId: Long
     ): BaseResponse<MyInterestResponseDto> =
         myService.getInterestAnimals(lastReportId, lastProtectId)
+
+    override suspend fun getReportHistory(lastReportId: Long): BaseResponse<MyReportHistoryResponseDto> =
+        myService.getReportHistory(lastReportId)
 }
