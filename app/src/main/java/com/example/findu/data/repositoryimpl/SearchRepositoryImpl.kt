76e022f1b540093c0544b7fd7676d4be20f6e9d4
IKSEONGKU <127182222+IKSEONGKU@users.noreply.github.com>
2025-02-19 18:09:03 +0900

    [feat] 조회 repository 쿼리용 파라미터 추가 추가

--- app/src/main/java/com/example/findu/data/repositoryimpl/SearchRepositoryImpl.kt
+++ app/src/main/java/com/example/findu/data/repositoryimpl/SearchRepositoryImpl.kt
@@ -11,24 +11,69 @@ class SearchRepositoryImpl @Inject constructor(
     private val searchRemoteDataSource: SearchRemoteDataSource
 ) : SearchRepository {
     override suspend fun getSearchAll(
+        startDate: String?,
+        endDate: String?,
+        species: String?,
+        breeds: String?,
+        location: String?,
         lastProtectId: Long,
         lastReportId: Long
     ): Result<List<SearchData>> =
         runCatching {
-            listOf(searchRemoteDataSource.getSearchAll(lastProtectId,lastReportId).handleBaseResponse().getOrThrow().toDomain())
+            listOf(
+                searchRemoteDataSource.getSearchAll(
+                    startDate = startDate,
+                    endDate = endDate,
+                    species = species,
+                    breeds = breeds,
+                    location = location,
+                    lastProtectId,
+                    lastReportId
+                ).handleBaseResponse().getOrThrow().toDomain()
+            )
         }
 
     override suspend fun getSearchReport(
+        startDate: String?,
+        endDate: String?,
+        species: String?,
+        breeds: String?,
+        location: String?,
         lastReportId: Long
     ): Result<List<SearchData>> =
         runCatching {
-            listOf(searchRemoteDataSource.getSearchReport(lastReportId).handleBaseResponse().getOrThrow().toDomain())
+            listOf(
+                searchRemoteDataSource.getSearchReport(
+                    startDate = startDate,
+                    endDate = endDate,
+                    species = species,
+                    breeds = breeds,
+                    location = location,
+                    lastReportId = lastReportId
+                ).handleBaseResponse()
+                    .getOrThrow().toDomain()
+            )
         }
 
     override suspend fun getSearchProtect(
+        startDate: String?,
+        endDate: String?,
+        species: String?,
+        breeds: String?,
+        location: String?,
         lastProtectId: Long
     ): Result<List<SearchData>> =
         runCatching {
-            listOf(searchRemoteDataSource.getSearchProtect(lastProtectId).handleBaseResponse().getOrThrow().toDomain())
+            listOf(
+                searchRemoteDataSource.getSearchProtect(
+                    startDate = startDate,
+                    endDate = endDate,
+                    species = species,
+                    breeds = breeds,
+                    location = location,
+                    lastProtectId = lastProtectId
+                ).handleBaseResponse()
+                    .getOrThrow().toDomain()
+            )
         }
 }
--- app/src/main/java/com/example/findu/domain/repository/SearchRepository.kt
+++ app/src/main/java/com/example/findu/domain/repository/SearchRepository.kt
@@ -4,15 +4,30 @@ import com.example.findu.domain.model.search.SearchData
 
 interface SearchRepository {
     suspend fun getSearchAll(
+        startDate: String?,
+        endDate: String?,
+        species: String?,
+        breeds: String?,
+        location: String?,
         lastProtectId: Long = Long.MAX_VALUE,
         lastReportId: Long = Long.MAX_VALUE
     ): Result<List<SearchData>>
 
     suspend fun getSearchReport(
+        startDate: String?,
+        endDate: String?,
+        species: String?,
+        breeds: String?,
+        location: String?,
         lastReportId: Long = Long.MAX_VALUE
     ): Result<List<SearchData>>
 
     suspend fun getSearchProtect(
+        startDate: String?,
+        endDate: String?,
+        species: String?,
+        breeds: String?,
+        location: String?,
         lastProtectId: Long = Long.MAX_VALUE
     ): Result<List<SearchData>>
 }
