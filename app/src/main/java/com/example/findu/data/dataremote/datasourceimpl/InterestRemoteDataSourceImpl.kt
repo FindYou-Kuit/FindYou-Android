327f46ae3486285e62d14a3e28711d78042a261e
IKSEONGKU <127182222+IKSEONGKU@users.noreply.github.com>
2025-02-19 23:58:48 +0900

    [feat] #36 관심 해제 service , datasource 구현

--- app/src/main/java/com/example/findu/data/dataremote/datasourceimpl/InterestRemoteDataSourceImpl.kt
+++ app/src/main/java/com/example/findu/data/dataremote/datasourceimpl/InterestRemoteDataSourceImpl.kt
@@ -1,6 +1,7 @@
 package com.example.findu.data.dataremote.datasourceimpl
 
 import com.example.findu.data.dataremote.datasource.InterestRemoteDataSource
+import com.example.findu.data.dataremote.model.base.NullableBaseResponse
 import com.example.findu.data.dataremote.service.InterestService
 import javax.inject.Inject
 
@@ -10,8 +11,12 @@ class InterestRemoteDataSourceImpl @Inject constructor(
     override suspend fun getInterestProtectingAnimals(id: Long) =
         interestService.getInterestProtectingAnimals(id)
 
-
     override suspend fun getInterestReportAnimals(id: Long) =
         interestService.getInterestReportAnimals(id)
 
+    override suspend fun deleteInterestProtectingAnimals(reportId: Long): NullableBaseResponse<Unit> =
+        interestService.deleteInterestProtectingAnimals(reportId)
+
+    override suspend fun deleteInterestReportAnimals(reportId: Long): NullableBaseResponse<Unit> =
+        interestService.deleteInterestReportAnimals(reportId)
 }
--- app/src/main/java/com/example/findu/data/dataremote/service/InterestService.kt
+++ app/src/main/java/com/example/findu/data/dataremote/service/InterestService.kt
@@ -2,7 +2,9 @@ package com.example.findu.data.dataremote.service
 
 import com.example.findu.data.dataremote.model.base.NullableBaseResponse
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface InterestService {
     @POST("/api/v1/users/interest-animals/protecting-animals")
@@ -14,4 +16,14 @@ interface InterestService {
     suspend fun getInterestReportAnimals(
         @Body id : Long
     ) : NullableBaseResponse<Int>
+
+    @DELETE("/api/v1/users/interest-animals/protecting-animals/{protecting_report_id}")
+    suspend fun deleteInterestProtectingAnimals(
+        @Path("protecting_report_id") reportId : Long
+    ) : NullableBaseResponse<Unit>
+
+    @DELETE("/api/v1/users/interest-animals/report-animals/{report_id}")
+    suspend fun deleteInterestReportAnimals(
+        @Path("report_id") reportId : Long
+    ) : NullableBaseResponse<Unit>
 }
