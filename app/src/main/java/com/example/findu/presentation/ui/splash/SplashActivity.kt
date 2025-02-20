ab5682f6a449aa53648d390678caf694d6868d9c
IKSEONGKU <127182222+IKSEONGKU@users.noreply.github.com>
2025-02-20 21:56:28 +0900

    [feat] 스플래시 화면 Gif 파일로 변경

--- app/src/main/java/com/example/findu/presentation/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/example/findu/presentation/ui/splash/SplashActivity.kt
@@ -1,9 +1,21 @@
 package com.example.findu.presentation.ui.splash
 
 import android.content.Intent
+import android.graphics.drawable.AnimatedImageDrawable
+import android.graphics.drawable.Drawable
+import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.load.resource.gif.GifDrawable
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.target.Target
+import com.bumptech.glide.request.transition.Transition
+import com.example.findu.R
 import com.example.findu.databinding.ActivitySplashBinding
 import com.example.findu.presentation.ui.login.LoginActivity
 import kotlinx.coroutines.delay
@@ -21,6 +33,35 @@ class SplashActivity : AppCompatActivity() {
             delay(3000)
             navigateToLogin()
         }
+
+        Glide.with(this)
+            .asGif()
+            .listener(object : RequestListener<GifDrawable> {
+                override fun onLoadFailed(
+                    e: GlideException?,
+                    model: Any?,
+                    target: Target<GifDrawable>,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    return false
+                }
+
+                override fun onResourceReady(
+                    resource: GifDrawable,
+                    model: Any,
+                    target: Target<GifDrawable>?,
+                    dataSource: DataSource,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    resource.setLoopCount(1)
+                    resource.stop()
+                    return false
+                }
+
+            })
+            .load(R.raw.splash)
+            .into(binding.ivSplashGif)
+
     }
 
     private fun navigateToLogin() {
