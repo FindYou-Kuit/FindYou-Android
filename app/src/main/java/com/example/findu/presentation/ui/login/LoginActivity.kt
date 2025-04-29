3a0ce1c5ef97a54316993be97aa88aa7afd1d4b5
t1nm1ksun <minsuk07672@naver.com>
2025-04-30 02:55:42 +0900

    [feat] #64: 로그인 UI 구현

--- app/src/main/java/com/example/findu/presentation/ui/login/LoginActivity.kt
+++ app/src/main/java/com/example/findu/presentation/ui/login/LoginActivity.kt
@@ -1,93 +1,25 @@
 package com.example.findu.presentation.ui.login
 
-import android.content.Intent
 import android.os.Bundle
-import android.text.InputType
-import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.lifecycleScope
-import com.example.findu.R
-import com.example.findu.databinding.ActivityLoginBinding
+import com.example.findu.presentation.ui.login.composeview.LoginScreen
 import com.example.findu.presentation.ui.login.viewmodel.LoginViewModel
-import com.example.findu.presentation.ui.main.MainActivity
-import com.example.findu.presentation.ui.signup.SignupActivity
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
-class LoginActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityLoginBinding
-    private var isPasswordVisible = false
+class LoginActivity : ComponentActivity() {
     private val loginViewModel: LoginViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityLoginBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        initListener()
-        observeLoginResult()
-    }
-
-    private fun initListener() {
-        binding.ivLoginPasswordVisible.setOnClickListener {
-            togglePasswordVisibility()
+        setContent {
+            LoginScreen(
+                kakaoLoginButtonClicked = {},
+                withoutSignUpButtonClicked = {},
+            )
         }
-
-        binding.tvLoginSignup.setOnClickListener {
-            val intent = Intent(this, SignupActivity::class.java)
-            startActivity(intent)
-        }
-
-        binding.clLoginButton.setOnClickListener {
-            validateLogin()
-        }
-    }
-
-    private fun togglePasswordVisibility() {
-        isPasswordVisible = !isPasswordVisible
-        if (isPasswordVisible) {
-            binding.etLoginPassword.inputType = InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
-            binding.ivLoginPasswordVisible.setImageResource(R.drawable.ic_password_on)
-        } else {
-            binding.etLoginPassword.inputType =
-                InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
-            binding.ivLoginPasswordVisible.setImageResource(R.drawable.ic_password_off)
-        }
-
-        binding.etLoginPassword.setSelection(binding.etLoginPassword.text.length)
-    }
-
-    private fun validateLogin() {
-        val email = binding.etLoginEmail.text.toString().trim()
-        val password = binding.etLoginPassword.text.toString().trim()
-
-        when {
-            email.isEmpty() -> showToast("이메일을 입력해주세요")
-            password.isEmpty() -> showToast("비밀번호를 입력해주세요")
-            else -> loginViewModel.postLogin(email, password)
-        }
-    }
-
-    private fun observeLoginResult() {
-        lifecycleScope.launch {
-            loginViewModel.loginResult.collect { result ->
-                result?.let {
-                    if (it) {
-                        showToast("로그인 성공!")
-                        startActivity(Intent(this@LoginActivity, MainActivity::class.java))
-                        finish()
-                    } else {
-                        showToast("로그인 실패! 계정을 확인해주세요.")
-                    }
-                }
-            }
-        }
-    }
-
-    private fun showToast(message: String) {
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
---
+++ app/src/main/java/com/example/findu/presentation/ui/login/composeview/LoginScreen.kt
@@ -0,0 +1,107 @@
+package com.example.findu.presentation.ui.login.composeview
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.findu.R
+import com.example.findu.presentation.util.extension.noRippleClickable
+import com.example.findu.presentation.util.extension.roundedBackgroundWithPadding
+import com.example.findu.ui.theme.FindUTheme
+
+@Composable
+fun LoginScreen(
+    kakaoLoginButtonClicked: () -> Unit,
+    withoutSignUpButtonClicked: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(color = FindUTheme.colors.white)
+            .padding(horizontal = 20.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Spacer(modifier = Modifier.weight(1.1f))
+        Text(
+            text = "작은 관심이" ,
+            style = FindUTheme.typography.head1SemiBold24,
+            color = FindUTheme.colors.gray6,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(5.dp))
+        Text(
+            text = "누군가의 세상이 됩니다" ,
+            style = FindUTheme.typography.head1SemiBold24,
+            color = FindUTheme.colors.gray6,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+        Text(
+            text = "유기동물 관련 어플리케이션",
+            style = FindUTheme.typography.body2Regular14,
+            color = FindUTheme.colors.gray4,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(34.dp))
+
+        Icon(
+            painter = painterResource(R.drawable.img_login_logo),
+            contentDescription = null,
+            tint = Color.Unspecified,
+        )
+        Spacer(modifier = Modifier.height(53.dp))
+        Icon(
+            painter = painterResource(R.drawable.img_kakao_login),
+            contentDescription = null,
+            modifier = Modifier.noRippleClickable(kakaoLoginButtonClicked),
+            tint = Color.Unspecified
+        )
+        Spacer(modifier = Modifier.height(15.dp))
+        Text(
+            "가입없이 시작하기",
+            textAlign = TextAlign.Center,
+            style = FindUTheme.typography.body1SemiBold16,
+            color = FindUTheme.colors.gray5,
+            modifier = Modifier
+                .fillMaxWidth()
+                .roundedBackgroundWithPadding(
+                    backgroundColor = FindUTheme.colors.gray2,
+                    cornerRadius = 8.dp,
+                    padding = PaddingValues(vertical = 15.dp)
+                )
+                .noRippleClickable(withoutSignUpButtonClicked)
+        )
+        Spacer(modifier = Modifier.weight(1.5f))
+
+    }
+}
+
+@Preview
+@Composable
+private fun LoginScreenPreview() {
+    LoginScreen(
+        kakaoLoginButtonClicked = {},
+        withoutSignUpButtonClicked = {},
+    )
+}
