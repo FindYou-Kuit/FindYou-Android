160e2f9508ed88ff990a0520ae94f2afd08ac4a4
t1nm1ksun <minsuk07672@naver.com>
2025-06-01 21:08:16 +0900

    [chore] #64: 키해시값 확인 함수 삭제

--- app/src/main/java/com/example/findu/FindUApp.kt
+++ app/src/main/java/com/example/findu/FindUApp.kt
@@ -17,28 +17,9 @@ class FindUApp : Application() {
     override fun onCreate() {
         super.onCreate()
         setKakao()
-        getKeyHash()
     }
 
     private fun setKakao() {
-        getKeyHash()
         KakaoSdk.init(this, KAKAO_NATIVE_APP_KEY)
-    }
-
-    private fun getKeyHash() {
-        Log.d("FindUApp1","getKeyHash key hash: ${Utility.getKeyHash(this)}")
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-            val packageInfo =
-                this.packageManager.getPackageInfo(this.packageName, PackageManager.GET_SIGNING_CERTIFICATES)
-            for (signature in packageInfo.signingInfo?.apkContentsSigners!!) {
-                try {
-                    val md = MessageDigest.getInstance("SHA")
-                    md.update(signature.toByteArray())
-                    Log.d("FindUApp","getKeyHash key hash: ${Base64.encodeToString(md.digest(), Base64.NO_WRAP)}")
-                } catch (e: NoSuchAlgorithmException) {
-                    Log.w("FindUApp","getKeyHash Unable to get MessageDigest. signature=$signature", e)
-                }
-            }
-        }
     }
 }
