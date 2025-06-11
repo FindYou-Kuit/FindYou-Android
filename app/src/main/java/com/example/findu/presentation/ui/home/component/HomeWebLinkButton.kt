dbfd6f8cd0edd550e41c81a5e1f71bf02f2d4a84
t1nm1ksun <minsuk07672@naver.com>
2025-06-11 16:30:30 +0900

    [feat] #67: 홈 하단 링크 버튼 구현

---
+++ app/src/main/java/com/example/findu/presentation/ui/home/component/HomeWebLinkButton.kt
@@ -0,0 +1,67 @@
+package com.example.findu.presentation.ui.home.component
+
+import android.content.Intent
+import android.net.Uri
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.findu.R
+import com.example.findu.presentation.ui.base.BaseVectorIcon
+import com.example.findu.presentation.util.extension.noRippleClickable
+import com.example.findu.presentation.util.extension.roundedBackgroundWithPadding
+import com.example.findu.ui.theme.FindUTheme
+
+@Composable
+fun HomeWebLinkButton(
+    buttonText: String,
+    webUrl: String,
+    modifier: Modifier = Modifier
+) {
+    val context = LocalContext.current
+
+    Row(
+        modifier = modifier
+            .border(width = 1.dp, color = FindUTheme.colors.mainColor, shape = RoundedCornerShape(20.dp))
+            .roundedBackgroundWithPadding(
+                backgroundColor = FindUTheme.colors.white,
+                cornerRadius = 20.dp,
+                padding = PaddingValues(vertical = 12.dp)
+            )
+            .noRippleClickable {
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(webUrl))
+                context.startActivity(intent)
+            },
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        BaseVectorIcon(vectorResource = R.drawable.ic_info_navigate)
+        Spacer(modifier = Modifier.width(10.dp))
+        Text(
+            text = buttonText,
+            style = FindUTheme.typography.body1SemiBold16,
+            color = FindUTheme.colors.mainColor
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun HomeWebLinkButtonPreview() {
+    HomeWebLinkButton(
+        modifier = Modifier.fillMaxWidth(),
+        buttonText = "동물정보보호시스템 바로가기",
+        webUrl = ""
+    )
+}
