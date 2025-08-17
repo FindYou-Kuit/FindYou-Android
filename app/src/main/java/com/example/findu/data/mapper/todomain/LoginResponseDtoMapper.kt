8d1f133e8b151b450f1eef2267da3cab7efdb537
t1nm1ksun <minsuk07672@naver.com>
2025-08-17 18:37:05 +0900

    [feat] #78: SignUp 유저정보 매퍼 구현

--- app/src/main/java/com/example/findu/data/mapper/todomain/LoginResponseDtoMapper.kt
+++ app/src/main/java/com/example/findu/data/mapper/todomain/LoginResponseDtoMapper.kt
@@ -7,11 +7,5 @@ import com.example.findu.domain.model.UserInfo
 fun LoginResponseDto.toDomain(): LoginData =
     LoginData(
         isFirstLogin = isFirstLogin,
-        userInfo = userInfo?.let {
-            UserInfo(
-                userId = it.userId,
-                nickname = it.nickname,
-                accessToken = it.accessToken
-            )
-        }
+        userInfo = userInfo?.toDomain()
     )
