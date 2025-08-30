f942fcb3768782af2ac675837511d28d59969abe
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2025-08-30 19:48:57 +0900

    [feat] #79 실종신고 Fragment 기본 세팅 완료

---
+++ app/src/main/java/com/example/findu/presentation/ui/report/missing/MissingReportScreen.kt
@@ -0,0 +1,24 @@
+package com.example.findu.presentation.ui.report.missing
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.findu.presentation.ui.report.missing.viewmodel.MissingReportUiEvent
+import com.example.findu.presentation.ui.report.missing.viewmodel.MissingReportUiState
+import com.example.findu.ui.theme.FindUTheme
+
+@Composable
+fun MissingReportScreen(
+    uiState: MissingReportUiState,
+    onEvent: (MissingReportUiEvent) -> Unit,
+) {
+
+}
+
+@Preview
+@Composable
+private fun MissingReportScreenPreview() {
+    FindUTheme {
+//        MissingReportScreen()
+    }
+}
--- app/src/main/java/com/example/findu/presentation/ui/report/missing/NewMissingReportFragment.kt
+++ app/src/main/java/com/example/findu/presentation/ui/report/missing/NewMissingReportFragment.kt
@@ -1,5 +1,7 @@
 package com.example.findu.presentation.ui.report.missing
 
+import android.R.attr.name
+import android.app.ProgressDialog.show
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,20 +11,21 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.flowWithLifecycle
-import com.example.findu.R
+import androidx.navigation.fragment.findNavController
 import com.example.findu.databinding.FragmentNewMissingReportBinding
-import com.example.findu.presentation.type.view.LoadState
-import com.example.findu.presentation.ui.home.composeview.HomeScreen
-import com.example.findu.presentation.ui.home.viewmodel.HomeUiEffect
-import com.example.findu.presentation.ui.home.viewmodel.HomeUiEvent
+import com.example.findu.presentation.ui.home.HomeFragmentDirections
+import com.example.findu.presentation.ui.report.missing.viewmodel.MissingReportUiEffect
+import com.example.findu.presentation.ui.report.missing.viewmodel.NewMissingReportViewModel
 
 class NewMissingReportFragment : Fragment() {
 
     private var _binding: FragmentNewMissingReportBinding? = null
     private val binding get() = _binding!!
+    private val viewModel by viewModels<NewMissingReportViewModel>()
 
 
     override fun onCreateView(
@@ -39,9 +42,39 @@ class NewMissingReportFragment : Fragment() {
         binding.missingReportComposeView.apply {
             setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
             setContent {
+                val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+                val lifecycleOwner = LocalLifecycleOwner.current
+                LaunchedEffect(viewModel.uiEffect, lifecycleOwner) {
+                    viewModel.uiEffect.flowWithLifecycle(
+                        lifecycle = lifecycleOwner.lifecycle
+                    ).collect { sideEffect ->
+                        when (sideEffect) {
+                            MissingReportUiEffect.NavigateToAddressSearch -> {}
+                            MissingReportUiEffect.NavigateToAnimalInfo -> {}
+                            is MissingReportUiEffect.ShowToast -> {
+                                Toast.makeText(
+                                    requireContext(), sideEffect.message, Toast.LENGTH_SHORT
+                                ).show()
+                            }
+                        }
+                    }
+                }
 
+                MissingReportScreen(
+                    uiState = uiState,
+                    onEvent = { event -> viewModel.handleEvent(event) },
+                )
             }
         }
     }
 
+    fun navigateToAddressSearch() {
+        findNavController().navigate(
+            HomeFragmentDirections.actionFragmentHomeToFragmentSearchDetailProtecting(
+//                id = id,
+//                tag = tag,
+//                name = name
+            )
+        )
+    }
 }
