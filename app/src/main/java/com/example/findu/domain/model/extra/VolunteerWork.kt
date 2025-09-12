7bb363756716354508ca9785d0dc1bde7b969d83
t1nm1ksun <minsuk07672@naver.com>
2025-09-12 19:25:49 +0900

    [feat] #85: 홈 추가 봉사정보 아이템 구현

---
+++ app/src/main/java/com/example/findu/domain/model/extra/VolunteerWork.kt
@@ -0,0 +1,10 @@
+package com.example.findu.domain.model.extra
+
+data class VolunteerWork(
+    val institution: String,
+    val recruitmentPeriod: String,
+    val address: String,
+    val workPeriod: String,
+    val workTime: String,
+    val webLink: String
+)
---
+++ app/src/main/java/com/example/findu/presentation/ui/extra/view/ExtraVolunteerScreen.kt
@@ -0,0 +1,26 @@
+package com.example.findu.presentation.ui.extra.view
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.findu.R
+import com.example.findu.presentation.ui.base.FindUTopAppBar
+
+@Composable
+fun ExtraHomeVolunteerScreen(modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        FindUTopAppBar(
+            title = R.string.home_extra_volunteer,
+            navigationIconRes = R.drawable.ic_arrow_left,
+            onNavigationIconClick = {}
+            )
+    }
+}
+
+@Preview
+@Composable
+private fun ExtraHomeVolunteerScreenPreview() {
+
+}
