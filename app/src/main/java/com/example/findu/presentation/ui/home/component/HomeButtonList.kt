e9d0670b53f3126422499d3b74c922c60ce192b5
t1nm1ksun <minsuk07672@naver.com>
2025-09-21 15:37:41 +0900

    [chore] #85: 스트링 포맷 최적화

--- app/src/main/java/com/example/findu/presentation/ui/home/component/HomeButtonList.kt
+++ app/src/main/java/com/example/findu/presentation/ui/home/component/HomeButtonList.kt
@@ -18,7 +18,6 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.findu.presentation.type.HomeExtraButtonType
-import com.example.findu.presentation.type.HomeExtraType
 import com.example.findu.presentation.util.extension.noRippleClickable
 import com.example.findu.ui.theme.FindUTheme
 
