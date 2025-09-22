50c4ec267aab375b12df1c74a61c639b76e89e96
t1nm1ksun <minsuk07672@naver.com>
2025-09-22 19:06:05 +0900

    [feat] #94: 홈 추가정보 데이터소스 구현

---
+++ app/src/main/java/com/example/findu/data/dataremote/datasource/InformationRemoteDataSource.kt
@@ -0,0 +1,25 @@
+package com.example.findu.data.dataremote.datasource
+
+import com.example.findu.data.dataremote.model.base.BaseResponse
+import com.example.findu.data.dataremote.model.response.information.VolunteersResponseDto
+import com.example.findu.data.dataremote.model.response.information.CentersResponseDto
+import com.example.findu.data.dataremote.model.response.information.DepartmentsResponseDto
+
+interface InformationRemoteDataSource {
+    suspend fun getVolunteers(
+        lastId: Long? = Long.MAX_VALUE,
+    ): BaseResponse<VolunteersResponseDto>
+
+    suspend fun getCenters(
+        lastId: Long? = Long.MAX_VALUE,
+        sido: String? = null,
+        sigungu: String? = null,
+        lat: Double? = null,
+        long: Double? = null
+    ): BaseResponse<CentersResponseDto>
+
+    suspend fun getDepartments(
+        district: String? = null,
+        lastId: Long? = Long.MAX_VALUE,
+    ): BaseResponse<DepartmentsResponseDto>
+}
