8b4fe2220e06561d4c64fa87df60c266c543d2db
t1nm1ksun <minsuk07672@naver.com>
2025-09-22 16:11:10 +0900

    [feat] #85: 클립 보드 복사 기능 추가

--- app/src/main/java/com/example/findu/presentation/ui/extra/component/ExtraDepartmentItem.kt
+++ app/src/main/java/com/example/findu/presentation/ui/extra/component/ExtraDepartmentItem.kt
@@ -9,16 +9,20 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.Divider
-import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalClipboardManager
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.core.net.toUri
+import com.example.findu.R
 import com.example.findu.domain.model.extra.Department
+import com.example.findu.presentation.ui.base.BaseVectorIcon
 import com.example.findu.presentation.util.extension.noRippleClickable
 import com.example.findu.ui.theme.FindUTheme
 
@@ -29,32 +33,44 @@ fun ExtraDepartmentItem(
 ) {
     val context = LocalContext.current
     val intent = Intent(Intent.ACTION_DIAL, "tel:${department.phone}".toUri())
+    val clipboardManager = LocalClipboardManager.current
 
 
     Column(modifier = modifier.padding(top = 20.dp, start = 20.dp, end = 20.dp)) {
-        Row {
+        Row(verticalAlignment = Alignment.CenterVertically) {
             Text(
                 text = department.name,
                 style = FindUTheme.typography.head2SemiBold20,
                 color = FindUTheme.colors.gray6
             )
             Spacer(modifier = Modifier.width(10.dp))
-            Icon(ve)
+            BaseVectorIcon(
+                vectorResource = R.drawable.ic_home_extra_department_copy_15,
+                modifier = Modifier.noRippleClickable {
+                    clipboardManager.setText(AnnotatedString(department.district))
+                })
             Text(
-                text ="복사",
+                text = "복사",
                 style = FindUTheme.typography.body2Regular14,
-                color = Color(0xFF00D1CA)
+                color = Color(0xFF00D1CA),
+                modifier = Modifier.noRippleClickable {
+                    clipboardManager.setText(AnnotatedString(department.district))
+                }
             )
         }
         Spacer(modifier = Modifier.height(10.dp))
-        Text(text = "Tel) ${department.phone} >", modifier = Modifier.noRippleClickable {
-            context.startActivity(intent)
-        }, style = FindUTheme.typography.body1Regular16,
-            color = FindUTheme.colors.blue1)
+        Text(
+            text = "Tel) ${department.phone} >", modifier = Modifier.noRippleClickable {
+                context.startActivity(intent)
+            }, style = FindUTheme.typography.body1Regular16,
+            color = FindUTheme.colors.blue1
+        )
         Spacer(modifier = Modifier.height(5.dp))
-        Text(text = department.district,
+        Text(
+            text = department.district,
             style = FindUTheme.typography.captionRegular12,
-            color = FindUTheme.colors.gray5)
+            color = FindUTheme.colors.gray5
+        )
         Spacer(modifier = Modifier.height(20.dp))
         Divider(
             modifier = Modifier.fillMaxWidth(),
