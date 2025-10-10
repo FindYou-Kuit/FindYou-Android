14d671b99abb45f8b7ba50aa980be49231ec7a44
t1nm1ksun <minsuk07672@naver.com>
2025-10-10 17:58:52 +0900

    [feat] #94 행정구역 드롭다운 구현

--- app/src/main/java/com/example/findu/presentation/ui/extra/view/ExtraDepartmentScreen.kt
+++ app/src/main/java/com/example/findu/presentation/ui/extra/view/ExtraDepartmentScreen.kt
@@ -1,15 +1,22 @@
 package com.example.findu.presentation.ui.extra.view
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.findu.R
 import com.example.findu.domain.model.extra.Department
+import com.example.findu.presentation.type.HomeExtraDistrictType
 import com.example.findu.presentation.ui.base.FindUTopAppBar
 import com.example.findu.presentation.ui.extra.component.ExtraDepartmentItem
+import com.example.findu.presentation.ui.extra.component.ExtraDistrictItem
 
 @Composable
 fun ExtraHomeDepartmentScreen(
@@ -22,8 +29,20 @@ fun ExtraHomeDepartmentScreen(
             navigationIconRes = R.drawable.ic_arrow_left,
             onNavigationIconClick = {}
         )
-        //TODO: 지역 선택 드롭다운 구현
+        Row (modifier = Modifier.padding(20.dp)){
+            ExtraDistrictItem(
+                districtType = HomeExtraDistrictType.DISTRICT_TYPE_SIDO,
+                onClick = {},
+                modifier = Modifier.weight(1f)
+            )
+            Spacer(modifier = Modifier.width(10.dp))
 
+            ExtraDistrictItem(
+                districtType = HomeExtraDistrictType.DISTRICT_TYPE_SIGUNGU,
+                onClick = {},
+                modifier = Modifier.weight(1f)
+            )
+        }
         LazyColumn {
             items(departments) {
                 ExtraDepartmentItem(department = it)
