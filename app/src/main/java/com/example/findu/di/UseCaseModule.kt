65b3d969d76cc7127d77b11153ed1f41085a507b
t1nm1ksun <minsuk07672@naver.com>
2025-10-10 15:20:01 +0900

    resolve confilct

--- app/src/main/java/com/example/findu/di/UseCaseModule.kt
+++ app/src/main/java/com/example/findu/di/UseCaseModule.kt
@@ -2,16 +2,13 @@ package com.example.findu.di
 
 import com.example.findu.domain.repository.AuthRepository
 import com.example.findu.domain.repository.BreedRepository
-import com.example.findu.domain.repository.UserInfoRepository
-import com.example.findu.domain.repository.report.ReportRepository
-import com.example.findu.domain.usecase.report.AnalysisImageWithGptUseCase
 import com.example.findu.domain.repository.DetailSearchRepository
-import com.example.findu.domain.repository.DeviceRepository
 import com.example.findu.domain.repository.HomeRepository
 import com.example.findu.domain.repository.InterestRepository
 import com.example.findu.domain.repository.MyRepository
 import com.example.findu.domain.repository.SearchRepository
 import com.example.findu.domain.repository.TokenRepository
+import com.example.findu.domain.repository.UserInfoRepository
 import com.example.findu.domain.repository.report.ReportRepository
 import com.example.findu.domain.usecase.GetBreedDataUseCase
 import com.example.findu.domain.usecase.GetBreedValidationUseCase
