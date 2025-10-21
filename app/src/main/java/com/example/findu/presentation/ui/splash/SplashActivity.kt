54f26172278f7a99769caaa3224b774b8b998457
t1nm1ksun <minsuk07672@naver.com>
2025-10-21 16:24:01 +0900

    [refactor] #94: 봉사 아이템 긴 텍스트 대응

--- app/src/main/java/com/example/findu/presentation/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/example/findu/presentation/ui/splash/SplashActivity.kt
@@ -2,6 +2,7 @@ package com.example.findu.presentation.ui.splash
 
 import android.content.Intent
 import android.os.Bundle
+import android.provider.Settings
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import com.bumptech.glide.Glide
@@ -12,13 +13,14 @@ import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.target.Target
 import com.example.findu.R
 import com.example.findu.databinding.ActivitySplashBinding
+import com.example.findu.domain.usecase.SetDeviceIdUseCase
 import com.example.findu.domain.usecase.token.GetAccessTokenUseCase
 import com.example.findu.presentation.ui.login.LoginActivity
 import com.example.findu.presentation.ui.main.MainActivity
 import dagger.hilt.android.AndroidEntryPoint
-import jakarta.inject.Inject
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class SplashActivity : AppCompatActivity() {
@@ -26,9 +28,17 @@ class SplashActivity : AppCompatActivity() {
     @Inject
     lateinit var getAccessTokenUseCase: GetAccessTokenUseCase
 
+    @Inject
+    lateinit var setDeviceIdUseCase: SetDeviceIdUseCase
+
     private lateinit var binding: ActivitySplashBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val deviceId = Settings.Secure.getString(this.contentResolver, Settings.Secure.ANDROID_ID)
+            ?: "unknown_device_id"
+
+        setDeviceIdUseCase(deviceId=deviceId)
 
         binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
