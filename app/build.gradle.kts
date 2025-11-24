5dc41ade1e30908facb0309f86e1ca9c7bdbd8b3
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2025-11-24 21:37:26 +0900

    [feat] #105 CameraCore 버전 업데이트

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.kuit.findu"
         minSdk = 28
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.0.3"
+        versionCode = 5
+        versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "GPT_KEY", properties["GPT_KEY"].toString())
