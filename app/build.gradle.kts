f2bff2439de71f4cd7cb834c1b0c4c1a2baceba3
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2025-12-11 17:50:19 +0900

    [chore] #107 앱 버전 업데이트
    - 앱 버전을 1.0.10에서 1.0.11로 업데이트
    - versionCode를 11에서 12로 업데이트

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.kuit.findu"
         minSdk = 28
         targetSdk = 35
-        versionCode = 11
-        versionName = "1.0.10"
+        versionCode = 12
+        versionName = "1.0.11"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "GPT_KEY", properties["GPT_KEY"].toString())
