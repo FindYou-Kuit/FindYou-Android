18182d558287ea90200f541dd60a373555b6bdd2
t1nm1ksun <minsuk07672@naver.com>
2025-12-29 22:35:00 +0900

    [chore] #111 Inject.Named 임포트 수정

--- app/src/main/java/com/kuit/findu/di/LogNetworkModule.kt
+++ app/src/main/java/com/kuit/findu/di/LogNetworkModule.kt
@@ -5,10 +5,10 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import jakarta.inject.Named
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Named
 import javax.inject.Singleton
 
 @Module
