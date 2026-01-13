3bfa6be8a052ed7f3f7d32852972d3d82b8daf5d
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2026-01-13 16:41:28 +0900

    [chore] 앱 버전 정보 업데이트
    - 앱의 `versionCode`를 12에서 13으로, `versionName`을 "1.0.11"에서 "1.0.12"로 업데이트했습니다.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.kuit.findu"
         minSdk = 28
         targetSdk = 35
-        versionCode = 12
-        versionName = "1.0.11"
+        versionCode = 13
+        versionName = "1.0.12"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "GPT_KEY", properties["GPT_KEY"].toString())
