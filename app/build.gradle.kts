0e7013a97e8cc730e1403d8d113c7d8b9b207974
ikseong00 <127182222+ikseong00@users.noreply.github.com>
2026-03-25 12:52:43 +0900

    [chore] 앱 버전 정보 업데이트 (1.1.8)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.kuit.findu"
         minSdk = 28
         targetSdk = 35
-        versionCode = 21
-        versionName = "1.1.6"
+        versionCode = 23
+        versionName = "1.1.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "GPT_KEY", properties["GPT_KEY"].toString())
